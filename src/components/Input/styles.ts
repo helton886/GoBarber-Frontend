c7992e6ba4b0897b0ac6e2d7b3e39324254533e2
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2021-01-08 00:16:28 -0300

    feature: Input component

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,45 @@
+import styled, { css } from 'styled-components';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  padding: 16px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  color: #666360;
+  & + div {
+    margin-top: 8px;
+  }
+
+  ${props =>
+    props.isFocused &&
+    css`
+      color: #ff9000;
+      border-color: #ff9000;
+    `}
+
+  ${props =>
+    props.isFilled &&
+    css`
+      color: #ff9000;
+    `}
+  input {
+    flex: 1;
+    border: 0;
+    color: #f4ede8;
+    background: transparent;
+    &::placeholder {
+      color: #666360;
+    }
+  }
+  svg {
+    margin-right: 16px;
+  }
+`;
