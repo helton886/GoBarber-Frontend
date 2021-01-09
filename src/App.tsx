24b926dedec3ae06a931618cb8217095471080c6
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2021-01-09 18:53:15 -0300

    chore: added hooks

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import GlobalStyle from './styles/global';
 import SignIn from '../src/pages/SignIn/signIn';
 import SignUp from '../src/pages/SignUp/signUp';
 
+import AppProvider from './hooks';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AppProvider>
+      <SignIn />
+    </AppProvider>
+
     <GlobalStyle />
   </>
 );
