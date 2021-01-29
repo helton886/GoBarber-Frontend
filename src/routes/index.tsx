00acd2c08a23d2611719f89448510ba76e607f06
Helton Vasconcelos <heltonvasconcelos88@gmail.com>
2021-01-29 00:04:52 -0300

    chore: refactoring import

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { Switch } from 'react-router-dom';
 import Route from './Route';
 
-import SignIn from '../pages/SignIn/signIn';
-import SignUp from '../pages/SignUp/signUp';
-import Dashboard from '../pages/Dashboard/';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import Dashboard from '../pages/Dashboard';
+import ForgotPassword from '../pages/ForgotPassword';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
     <Route path="/signup" component={SignUp} />
+    <Route path="/forgot-password" component={ForgotPassword} />
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
 );
